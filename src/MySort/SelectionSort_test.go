1f7b3a77c81c8450bb778d391a70ada57fe93861
cipepser <respepic@gmail.com>
2017-01-21 22:45:36 +0900

    add benchmark of sorted and randomized data for SelectionSort

--- src/MySort/SelectionSort_test.go
+++ src/MySort/SelectionSort_test.go
@@ -27,3 +27,22 @@ func BenchmarkSelectionSort(b *testing.B) {
 		SelectionSort(tmp)
 	}
 }
+
+func BenchmarkSelectionSorted(b *testing.B) {
+	tmp := make([]int, len(SortedData))
+
+	for i:= 0; i <b.N; i++ {
+		copy(tmp, SortedData)
+
+		SelectionSort(tmp)
+	}
+}
+
+func BenchmarkSelectionSortRandomizedData(b *testing.B) {
+	for i:= 0; i <b.N; i++ {
+		rand.Seed(time.Now().UnixNano())
+		tmp := rand.Perm(len(a))
+		
+		SelectionSort(tmp)
+	}
+}
