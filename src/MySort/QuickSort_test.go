bd237b556c257e9b8bb71f71d0445fba1f9a5c9c
cipepser <respepic@gmail.com>
2017-01-21 22:45:10 +0900

    add benchmark of sorted and randomized data for QuickSort

--- src/MySort/QuickSort_test.go
+++ src/MySort/QuickSort_test.go
@@ -27,3 +27,22 @@ func BenchmarkMQuickSort(b *testing.B) {
 		QuickSort(tmp)
 	}
 }
+
+func BenchmarkQuickSorted(b *testing.B) {
+	tmp := make([]int, len(SortedData))
+
+	for i:= 0; i <b.N; i++ {
+		copy(tmp, SortedData)
+
+		QuickSort(tmp)
+	}
+}
+
+func BenchmarkQuickSortRandomizedData(b *testing.B) {
+	for i:= 0; i <b.N; i++ {
+		rand.Seed(time.Now().UnixNano())
+		tmp := rand.Perm(len(a))
+		
+		QuickSort(tmp)
+	}
+}
