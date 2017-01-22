f919fe940649945c0aaa5224d03593be8f519aa8
cipepser <respepic@gmail.com>
2017-01-22 14:36:56 +0900

    add comments for offset of copying and generating randomized data

--- src/MySort/Base_test.go
+++ src/MySort/Base_test.go
@@ -6,6 +6,7 @@ import (
 	"time"
 )
 
+// コピーのoffset
 func BenchmarkBaseCopy(b *testing.B) {
 	tmp := make([]int, len(a))
 
@@ -14,8 +15,8 @@ func BenchmarkBaseCopy(b *testing.B) {
 	}
 }
 
+// 乱数生成のoffset
 func BenchmarkBaseRandom(b *testing.B) {
-
 	for i:= 0; i <b.N; i++ {
 		rand.Seed(time.Now().UnixNano())
 		rand.Perm(len(a))
